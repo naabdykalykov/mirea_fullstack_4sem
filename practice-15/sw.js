98b5b8e81f9bf33b9d322d3bd61796199cf129f7
Nursultan Abdykalykov <znslt24@gmail.com>
2026-03-30 18:43:26 +0300

    add practice-15

---
+++ practice-15/sw.js
@@ -0,0 +1,71 @@
+﻿const APP_SHELL_CACHE = "practice15-app-shell-v1";
+const DYNAMIC_CACHE = "practice15-dynamic-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./content/home.html",
+  "./content/about.html",
+  "./icons/favicon.ico",
+  "./icons/icon-16.png",
+  "./icons/icon-32.png",
+  "./icons/icon-64.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(APP_SHELL_CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== APP_SHELL_CACHE && key !== DYNAMIC_CACHE)
+            .map((key) => caches.delete(key)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+
+  if (url.pathname.startsWith("/content/")) {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkRes) => {
+          const copy = networkRes.clone();
+          caches
+            .open(DYNAMIC_CACHE)
+            .then((cache) => cache.put(event.request, copy));
+          return networkRes;
+        })
+        .catch(() =>
+          caches
+            .match(event.request)
+            .then((cached) => cached || caches.match("./content/home.html")),
+        ),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((cached) => cached || fetch(event.request)),
+  );
+});
